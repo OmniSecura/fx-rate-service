ebd0c26355eecb5812328d7159d9ae3b8da2851f
Joanna Zawislak <joannazawislak549@gmail.com>
2026-04-03 12:35:11 +0000

    new script - db_dump.sh

---
+++ sprint1/scripts/db_dump.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# db_dump.sh: Full schema and data dump for MySQL database
+# Usage: ./db_dump.sh [database_name] [output_file]
+# Defaults: database_name=fx_rate_service, output_file=fx_rate_dump.sql
+
+MYSQL_CMD="/c/Program Files/MySQL/MySQL Server 8.0/bin/mysql"
+MYSQLDUMP_CMD="/c/Program Files/MySQL/MySQL Server 8.0/bin/mysqldump"
+
+DB_NAME=${1:-fx_market_data}
+OUTPUT_FILE=${2:-fx_rate_dump.sql}
+
+echo "Dumping full schema and data from database '$DB_NAME' to '$OUTPUT_FILE'..."
+
+"$MYSQLDUMP_CMD" -u root -p --routines --triggers --single-transaction "$DB_NAME" > "$OUTPUT_FILE"
+
+if [ $? -eq 0 ]; then
+    echo "Dump completed successfully: $OUTPUT_FILE"
+else
+    echo "Error: Dump failed."
+    exit 1
+fi
