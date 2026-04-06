693f48f56222ce408097b2a39dee802aa4f17e16
Joanna Zawislak <joannazawislak549@gmail.com>
2026-04-06 09:49:19 +0000

    db_dump.sh, database name changed to fx_rate_db

--- sprint1/scripts/db_dump.sh
+++ sprint1/scripts/db_dump.sh
@@ -2,11 +2,11 @@
 
 # db_dump.sh: Full schema and data dump for MySQL database
 # Usage: ./db_dump.sh [database_name] [output_file]
-# Defaults: database_name=fx_rate_service, output_file=fx_rate_dump.sql
+# Defaults: database_name=fx_rate_db, output_file=fx_rate_dump.sql
 
 MYSQLDUMP_CMD="/c/Program Files/MySQL/MySQL Server 8.0/bin/mysqldump"
 
-DB_NAME=${1:-fx_market_data}
+DB_NAME=${1:-fx_rate_db}
 OUTPUT_FILE=${2:-fx_rate_dump.sql}
 
 echo "Dumping full schema and data from database '$DB_NAME' to '$OUTPUT_FILE'..."
