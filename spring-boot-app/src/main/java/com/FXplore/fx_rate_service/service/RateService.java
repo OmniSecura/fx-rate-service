ea44a5a7ad404d268cb3a802d165c6468fb23dbf
kaktus549 <joannazawislak549@gmail.com>
2026-04-13 15:55:17 +0200

    Created RateService interface.

--- spring-boot-app/src/main/java/com/FXplore/fx_rate_service/service/RateService.java
+++ spring-boot-app/src/main/java/com/FXplore/fx_rate_service/service/RateService.java
@@ -1,4 +1,42 @@
 package com.FXplore.fx_rate_service.service;
 
-public class RateService {
+import com.FXplore.fx_rate_service.model.CurrencyPair;
+import com.FXplore.fx_rate_service.model.EodFixing;
+import com.FXplore.fx_rate_service.model.ExchangeRate;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public class RateService implements IRateService{
+    @Override
+    public void storeRate(ExchangeRate rate) {
+
+    }
+
+    @Override
+    public Optional<ExchangeRate> getLatestRate(CurrencyPair pair) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<ExchangeRate> getRateHistory(CurrencyPair pair, LocalDate from, LocalDate to) {
+        return List.of();
+    }
+
+    @Override
+    public Optional<BigDecimal> calculateCrossRate(CurrencyPair pairAC, CurrencyPair pairAB, CurrencyPair pairBC) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<BigDecimal> convertAmount(BigDecimal amount, CurrencyPair pair) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<EodFixing> getEodFixing(CurrencyPair pair, LocalDate date) {
+        return Optional.empty();
+    }
 }
